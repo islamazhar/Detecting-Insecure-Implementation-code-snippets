82a344ef42518bca825fc31b8058cca2d948fab9
Mazharul Islam <mislam9@wisc.edu>
2020-12-06 22:26:37 -0600

    Added writing

--- PPA/src/examples/AllCodeSnippets/class_1000.java
+++ PPA/src/examples/AllCodeSnippets/class_1000.java
@@ -1,8 +1,8 @@
 package examples.AllCodeSnippets; 
 public class class_1000{ 
- public static void main() { 
-MessageDigest digest = MessageDigest.getInstance("MD5");
-digest.update(inputPassword.getBytes());
-byte[] hashedPassword = digest.digest();
+ public static void main() {
+		MessageDigest digest = MessageDigest.getInstance("MD5");
+		digest.update(inputPassword.getBytes());
+		byte[] hashedPassword = digest.digest();
   }
 }
